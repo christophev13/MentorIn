4522d2efee985908b95380b23d86265fd3f0c93f
francois guerin <francois.theo.guerin@outlook.com>
2017-04-18 12:09:56 -0300

    Booking model ready with validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,18 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :meeting
+
+  validates :user, presence: true
+  #exclusion: { in: meeting.user, message: "You can not attend your own mentorship" }
+  validates :meeting, presence: true
+  validates :motivations, presence: true
+
+  validate :mentor_cant_be_student
+
+  private
+  def mentor_cant_be_student
+    if self.user == self.meeting.user
+      errors.add(:user, " can not attend your own mentorship")
+    end
+  end
 end
