321d40b61969cf622e45e94462ba2466ae126142
Pedro Koike <ps.koike@gmail.com>
2017-04-19 16:21:21 -0300

    home page designed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :meeting
 
   validates :user, presence: true
+  validates :user, uniqueness: { scope: :meeting, message: "you can sign-up only once for mentorship event" }
   #exclusion: { in: meeting.user, message: "You can not attend your own mentorship" }
   validates :meeting, presence: true
   validates :motivations, presence: true
@@ -11,8 +12,6 @@ class Booking < ApplicationRecord
 
   private
   def mentor_cant_be_student
-    if self.user == self.meeting.user
-      errors.add(:user, " can not attend your own mentorship")
-    end
+      errors.add(:user, " can not attend your own mentorship") if self.user == self.meeting.user
   end
 end
