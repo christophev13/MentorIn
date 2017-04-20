06cf78a66695169819d82c304640e5b32b99ab51
francois guerin <francois.theo.guerin@outlook.com>
2017-04-20 17:12:31 -0300

    button done/map to do

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,4 +5,7 @@ class DashboardsController < ApplicationController
     @nb_meeting_mentor = @user.meetings.count
     @nb_meeting_mentee = @user.booking.count
   end
+
+
+
 end
--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,2 +1,11 @@
 module DashboardsHelper
+
+  def mentee_confirmed(meeting)
+    meeting.booking.reduce(0){|counter, item| counter += (item.confirmation == true ? 1 : 0)}
+  end
+
+  def mentee_pending(meeting)
+    meeting.booking.reduce(0){|counter, item| counter += (item.confirmation == true ? 0 : 1)}
+  end
+
 end
