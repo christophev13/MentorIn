ac6a85a6f4eab00b7245fd76b0a54ba4d02eb40f
christophe vallerin <christophe.vallerin@hotmail.com>
2017-04-20 16:39:58 -0300

    added linkedin sign up, only missing profile push

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,156 @@
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :meetings, dependent: :destroy
   has_many :booking
 
-  INDUSTRY = ["Accommodations", "Accounting", "Advertising", "Aerospace", "Agriculture & Agribusiness", "Air Transportation", "Apparel & Accessories", "Auto", "Banking", "Beauty & Cosmetics", "Biotechnology", "Chemical", "Communications", "Computer", "Construction", "Consulting", "Consumer Products", "Education", "Electronics", "Employment", "Energy", "Entertainment & Recreation", "Fashion", "Financial Services", "Fine Arts", "Food & Beverage", "Green Technology", "Health", "Information", "Information Technology", "Insurance", "Journalism & News", "Legal Services", "Manufacturing", "Media & Broadcasting", "Medical Devices & Supplies", "Motion Pictures & Video", "Music", "Pharmaceutical", "Public Administration", "Public Relations", "Publishing", "Rail", "Real Estate", "Retail", "Service", "Sports", "Technology", "Telecommunications", "Tourism", "Transportation", "Travel", "Utilities", "Video Game", "Web Services"]
+  INDUSTRY = ["Airlines/Aviation",
+"Alternative Dispute Resolution",
+"Alternative Medicine",
+"Animation",
+"Apparel & Fashion",
+"Architecture & Planning",
+"Arts and Crafts",
+"Automotive",
+"Aviation & Aerospace",
+"Banking",
+"Biotechnology",
+"Broadcast Media",
+"Building Materials",
+"Business Supplies and Equipment",
+"Capital Markets",
+"Chemicals",
+"Civic & Social Organization",
+"Civil Engineering",
+"Commercial Real Estate",
+"Computer & Network Security",
+"Computer Games",
+"Computer Hardware",
+"Computer Networking",
+"Computer Software",
+"Construction",
+"Consumer Electronics",
+"Consumer Goods",
+"Consumer Services",
+"Cosmetics",
+"Dairy",
+"Defense & Space",
+"Design",
+"Education Management",
+"E-Learning",
+"Electrical/Electronic Manufacturing",
+"Entertainment",
+"Environmental Services",
+"Events Services",
+"Executive Office",
+"Facilities Services",
+"Farming",
+"Financial Services",
+"Fine Art",
+"Fishery",
+"Food & Beverages",
+"Food Production",
+"Fund-Raising",
+"Furniture",
+"Gambling & Casinos",
+"Glass, Ceramics & Concrete",
+"Government Administration",
+"Government Relations",
+"Graphic Design",
+"Health, Wellness and Fitness",
+"Higher Education",
+"Hospital & Health Care",
+"Hospitality",
+"Human Resources",
+"Import and Export",
+"Individual & Family Services",
+"Industrial Automation",
+"Information Services",
+"Information Technology and Services",
+"Insurance",
+"International Affairs",
+"International Trade and Development",
+"Internet",
+"Investment Banking",
+"Investment Management",
+"Judiciary",
+"Law Enforcement",
+"Law Practice",
+"Legal Services",
+"Legislative Office",
+"Leisure, Travel & Tourism",
+"Libraries",
+"Logistics and Supply Chain",
+"Luxury Goods & Jewelry",
+"Machinery",
+"Management Consulting",
+"Maritime",
+"Market Research",
+"Marketing and Advertising",
+"Mechanical or Industrial Engineering",
+"Media Production",
+"Medical Devices",
+"Medical Practice",
+"Mental Health Care",
+"Military",
+"Mining & Metals",
+"Motion Pictures and Film",
+"Museums and Institutions",
+"Music",
+"Nanotechnology",
+"Newspapers",
+"Non-Profit Organization Management",
+"Oil & Energy",
+"Online Media",
+"Outsourcing/Offshoring",
+"Package/Freight Delivery",
+"Packaging and Containers",
+"Paper & Forest Products",
+"Performing Arts",
+"Pharmaceuticals",
+"Philanthropy",
+"Photography",
+"Plastics",
+"Political Organization",
+"Primary/Secondary Education",
+"Printing",
+"Professional Training & Coaching",
+"Program Development",
+"Public Policy",
+"Public Relations and Communications",
+"Public Safety",
+"Publishing",
+"Railroad Manufacture",
+"Ranching",
+"Real Estate",
+"Recreational Facilities and Services",
+"Religious Institutions",
+"Renewables & Environment",
+"Research",
+"Restaurants",
+"Retail",
+"Security and Investigations",
+"Semiconductors",
+"Shipbuilding",
+"Sporting Goods",
+"Sports",
+"Staffing and Recruiting",
+"Supermarkets",
+"Telecommunications",
+"Textiles",
+"Think Tanks",
+"Tobacco",
+"Translation and Localization",
+"Transportation/Trucking/Railroad",
+"Utilities",
+"Venture Capital & Private Equity",
+"Veterinary",
+"Warehousing",
+"Wholesale",
+"Wine and Spirits",
+"Wireless", "Writing and Editing"]
+
   PROFILE = ["College Freshman", "College Senior", "Recent Graduate", "Junior Professional", "Professional", "Expert Professional"]
 
   devise :database_authenticatable, :registerable,
@@ -15,35 +161,37 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :industry, presence: true, inclusion:{in: INDUSTRY}
-  validates :profile, presence: true, inclusion:{in: PROFILE}
+  #validates :profile, presence: true, inclusion:{in: PROFILE}
   validates :biography, presence: true
 
 
   def self.find_for_linkedin_oauth(auth)
 
+    binding.pry
+    user_params = auth.slice(:provider, :uid)
+    user_params.merge! auth.info.slice(:email, :first_name, :last_name)
+    user_params[:avatar_url] = auth.info.image
+    user_params[:linkedin_url] = auth.info.urls.public_profile
+    user_params[:industry] =auth.info.industry
+    user_params[:biography] = auth.extra.raw_info.summary
+    user_params[:token] = auth.credentials.token
+    user_params
+    user_params = user_params.to_h
 
-    byebug
-    puts auth
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+    user ||= User.where(email: auth.info.email).first # User did a regular sign up in the past.
+    if user
+      user.update(user_params)
+    else
+      user = User.new(user_params)
+      user.password = Devise.friendly_token[0,20]  # Fake password for validation
+      user.save
+    end
 
-    # user_params = auth.slice(:provider, :uid)
-    # user_params.merge! auth.info.slice(:email, :first_name, :last_name)
-    # user_params[:linkedin_picture_url] = auth.info.image
-    # user_params[:token] = auth.credentials.token
-    # user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
-    # user_params = user_params.to_h
-
-    # user = User.where(provider: auth.provider, uid: auth.uid).first
-    # user ||= User.where(email: auth.info.email).first # User did a regular sign up in the past.
-    # if user
-    #   user.update(user_params)
-    # else
-    #   user = User.new(user_params)
-    #   user.password = Devise.friendly_token[0,20]  # Fake password for validation
-    #   user.save
-    # end
-
-    # return user
+    return user
   end
 
 
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420155642) do
+ActiveRecord::Schema.define(version: 20170420182204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,6 @@ ActiveRecord::Schema.define(version: 20170420155642) do
     t.string   "provider"
     t.string   "uid"
     t.string   "token"
-    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
